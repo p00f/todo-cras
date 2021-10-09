0e08e4a2cb4c646da67146d0d7811b3274a80154
Chinmay Dalal <dalal.chinmay.0101@gmail.com>
2021-10-09 16:55:20 +0530

    Add LICENSE

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,22 @@
+/*
+  Copyright (C) 2021 Chinmay Dalal
+
+  This file is part of todo-cras.
+
+  todo-cras is free software: you can redistribute it and/or modify
+  it under the terms of the GNU General Public License as published by
+  the Free Software Foundation, either version 3 of the License, or
+  (at your option) any later version.
+
+  todo-cras is distributed in the hope that it will be useful,
+  but WITHOUT ANY WARRANTY; without even the implied warranty of
+  MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+  GNU General Public License for more details.
+
+  You should have received a copy of the GNU General Public License
+  along with todo-cras.  If not, see <https://www.gnu.org/licenses/>.
+*/
+
 use todo_cras::{display, edit_mode, help, ok_or_exit, read};
 
 use home::home_dir;
